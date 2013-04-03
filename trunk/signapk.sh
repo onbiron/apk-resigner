f3eeee8df5020e3d12eb89fe31c4c5d54a2836a1
onbiron1110@gmail.com <onbiron1110@gmail.com@d5d56db4-8b25-b229-cfbe-5ddd3a5ad5a6>
2013-04-03 09:35:12 +0000

    First commit

---
+++ trunk/signapk.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Sample usage is as follows;
+# ./signapk myapp.apk debug.keystore android androiddebugkey
+# 
+# param1, APK file: Calculator_debug.apk
+# param2, keystore location: ~/.android/debug.keystore
+# param3, key storepass: android
+# param4, key alias: androiddebugkey
+
+APK=$1
+KEYSTORE=$2
+STOREPASS=$3
+ALIAS=$4
+
+SIGNED_APK="signed_"$APK
+
+#debug
+#echo param1 $APK
+#echo param2 $KEYSTORE
+#echo param3 $STOREPASS
+#echo param4 $SIGNED_APK
+
+# delete META-INF folder
+zip -d $APK META-INF/\*
+
+# sign APK
+jarsigner -verbose -keystore $KEYSTORE -storepass $STOREPASS $APK $ALIAS
+#verify
+jarsigner -verify $APK
+
+#zipalign
+zipalign -v 4 $APK $SIGNED_APK 
